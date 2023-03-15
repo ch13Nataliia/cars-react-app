6309c6faf4918886782343ef605bc6db270b199a
Nataliia Tsirul <tcirulnatalya@gmail.com>
2023-03-15 10:36:43 +0000

    UI navigation added

--- src/components/DeskNav.jsx
+++ src/components/DeskNav.jsx
@@ -1,9 +1,54 @@
-import React from 'react'
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
 
-function DeskNav() {
+import IconButton from '@mui/material/IconButton';
+
+import MenuIcon from '@mui/icons-material/Menu';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import { NavLink } from 'react-router-dom';
+import { useTheme } from '@mui/material/styles';
+function DeskNav({
+  
+  handleDrawerToggle = () =>
+    console.log('No handleDrawToggle function provided'),
+}) {
+
+  const theme = useTheme();
+console.log(theme)
+
+const lightTextColor = theme.palette.common.white;
+
   return (
-    <div>DeskNav</div>
-  )
+    <AppBar component="nav">
+      <Toolbar>
+        <IconButton
+          color="inherit"
+          aria-label="open drawer"
+          edge="start"
+          onClick={handleDrawerToggle}
+          sx={{ mr: 2, display: { sm: 'none' } }}
+        >
+          <MenuIcon />
+        </IconButton>
+        <Typography
+          variant="h6"
+          component={NavLink}
+          to="/"
+          sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' }, textDecoration: "none", color: lightTextColor }}
+        >
+          Cars App
+        </Typography>
+        <Box sx={{ display: { xs: 'none', sm: 'block' } } }>
+          <Button sx={{ color: lightTextColor }} component={NavLink} to="/add">
+            Add Car
+          </Button>
+        </Box>
+      </Toolbar>
+    </AppBar>
+  );
 }
 
-export default DeskNav
+export default DeskNav;
--- src/components/MobNav.jsx
+++ src/components/MobNav.jsx
@@ -1,9 +1,55 @@
-import React from 'react'
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Divider from '@mui/material/Divider';
+import Drawer from '@mui/material/Drawer';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+import Typography from '@mui/material/Typography';
+import { NavLink } from 'react-router-dom';
 
-function MobNav() {
+function MobNav({
+  mobileOpen = false,
+  handleDrawerToggle = () =>
+    console.log('No handleDrawToggle function provided'),
+  drawerWidth = 240,
+}) {
   return (
-    <div>MobNav</div>
-  )
+    <Box component="nav">
+      <Drawer
+        variant="temporary"
+        open={mobileOpen}
+        onClose={handleDrawerToggle}
+        ModalProps={{
+          keepMounted: true, // Better open performance on mobile.
+        }}
+        sx={{
+          display: { xs: 'block', sm: 'none' },
+          '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+        }}
+      >
+        <Box onClick={handleDrawerToggle} sx={{ textAlign: 'center' }}>
+          <Typography variant="h6" sx={{ my: 2 }}>
+            Cars APP
+          </Typography>
+          <Divider />
+          <List>
+            <ListItem>
+              <ListItemButton sx={{ textAlign: 'left' }} component={NavLink} to="/">
+                <ListItemText primary={'List Cars'} />
+              </ListItemButton>
+            </ListItem>
+            <ListItem>
+              <ListItemButton sx={{ textAlign: 'left' }} component={NavLink} to="/add">
+                <ListItemText primary={'Add Car'} />
+              </ListItemButton>
+            </ListItem>
+          </List>
+        </Box>
+      </Drawer>
+    </Box>
+  );
 }
 
-export default MobNav
+export default MobNav;
